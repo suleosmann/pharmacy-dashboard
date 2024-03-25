e0ede520e7c7f557d5be6fa85343db810163594c
Suleiman <suleosman73@gmail.com>
2024-03-25 16:39:33 +0300

    Added gitignore

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DoctorForm } from "./DoctorForm";
-export { default as PatientForm } from "./PatientForm";
-export { default as BookAppointment } from "./BookAppointment";
-export { default as AppointmentsList} from "./AppointmentsList";
